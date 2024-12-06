58a30cdd4818e623559d29b26643123327336c58
Mohammed Bajaman <mohammed_bajaman@hotmail.com>
2024-12-06 21:11:52 +0300

    Updated NavBar with minimal modern design

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,46 +1,59 @@
 import React, { useState } from 'react'
 import './Navbar.css'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const [showMenu, setShowMenu] = useState(false);
+    const navItems = [
+        { path: '/', label: 'HOME' },
+        { path: '/portfolio', label: 'WORK' },
+        { path: '/about', label: 'ABOUT' }
+    ];
+
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
     return (
-        <nav className='nav'>
-            <div className='nav__desktop'>
-                <Link className='nav__title-link' to='/'>
-                    <b>MOHAMMED BAJAMAN</b>
+        <nav className='navbar'>
+            <div className='navbar__desktop'>
+                <Link className='navbar__brand' to='/'>
+                    <span className='navbar__logo'>MOHAMMED BAJAMAN</span>
                 </Link>
-                <div className='nav__desktop-menu'>
-                    <Link to='/' spy="true" smooth="true" offset={-50} duration={500} className='nav__desktop-menu-item'>HOME</Link>
-                    <Link to='/portfolio' spy="true" smooth="true" offset={-50} duration={500} className='nav__desktop-menu-item'>PORTFOLIO</Link>
-                    <Link to='/' spy="true" smooth="true" offset={-50} duration={500} className='nav__desktop-menu-item'>BLOG (Coming Soon)</Link>
+                
+                <div className='navbar__menu'>
+                    {navItems.map((item) => (
+                        <Link 
+                            key={item.label}
+                            to={item.path}
+                            className='navbar__menu-item'
+                        >
+                            {item.label}
+                        </Link>
+                    ))}
                 </div>
+            </div>
 
+            <div className='navbar__mobile'>
+                <Link className='navbar__brand' to='/'>
+                    <span className='navbar__logo'>MB</span>
+                </Link>
 
+                <button 
+                    className={`navbar__hamburger ${isMenuOpen ? 'active' : ''}`}
+                    onClick={toggleMenu}
+                    aria-label="Toggle menu"
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
 
+                <div className={`navbar__mobile-menu ${isMenuOpen ? 'active' : ''}`}>
                 {/* <Link className='nav__desktop-logo' to='/'>
                     <img alt='Logo' src={Logo} />
                 </Link> */}
             </div>
-
-            <div className='nav__mob'>
-            <svg className={showMenu ? 'nav__mob-button-svg menu-open' : 'nav__mob-button-svg'} onClick={()=>setShowMenu(!showMenu)}>
-                <line x1="0" y1="50%" x2="100%" y2="50%" className="top" shapeRendering="crispEdges" />
-                <line x1="0" y1="50%" x2="100%" y2="50%" className="middle" shapeRendering="crispEdges" />
-                <line x1="0" y1="50%" x2="100%" y2="50%" className="bottom" shapeRendering="crispEdges" />
-            </svg>
-
-            <Link className='nav__mob-logo' to='/'>
-                {/* <img alt='Logo' src={Logo} /> */}
-            </Link>
-            
-            <div className={`nav__mob-items${showMenu ? ' active' : ''}`}>
-                <Link to='/' spy="true" smooth="true" offset={-50} duration={500} className ='nav__mob-menu-item' onClick={()=>setShowMenu(false)}>HOME</Link>
-                <Link to='/portfolio' spy="true" smooth="true" offset={-50} duration={500} className ='nav__mob-menu-item' onClick={()=>setShowMenu(false)}>WORK</Link>
-            </div>
-            </div>
+        </div>
         </nav>
     )
 }
